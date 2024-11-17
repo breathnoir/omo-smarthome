50f62a96b8fcff3ad674e2392426727b387ff4a7
nadezhda <zakopnad@fel.cvut.cz>
2024-11-17 23:56:51 +0100

    reading config file in Start

--- sh/src/main/java/smarthome/Start.java
+++ sh/src/main/java/smarthome/Start.java
@@ -1,7 +1,32 @@
 package smarthome;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import smarthome.config.HouseConfig;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Start {
     public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
 
+        String configFile = "houseconfig.json";
+
+        try {
+            InputStream inputStream = Start.class.getClassLoader().getResourceAsStream(configFile);
+
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource not found: " + configFile);
+            }
+
+            HouseConfig houseConfig = mapper.readValue(inputStream, HouseConfig.class);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
