be6bfc4c823c7dfe7b7cd811e9952b973b0d28ca
Illia Zakharchenko <zakhaill@fel.cvut.cz>
2024-11-18 00:11:52 +0100

    room, floor, house classes modified

--- sh/src/main/java/smarthome/entities/Room.java
+++ sh/src/main/java/smarthome/entities/Room.java
@@ -1,4 +1,35 @@
 package smarthome.entities;
 
+import smarthome.entities.devices.Device;
+import smarthome.entities.sensors.Sensor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+    private String name;
+    private List<Device> devices = new ArrayList<>();
+    private List<Sensor> sensors = new ArrayList<>();
+
+    public Room(String roomName) {
+        this.name = roomName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addDevice(Device device) {
+        devices.add(device);
+    }
+
+    public void addSensor(Sensor sensor) {
+        sensors.add(sensor);
+    }
+
+
 }
