7d92b6224ff4e40f40df2b7d7d632eee7766a49d
nadezhda <zakopnad@fel.cvut.cz>
2025-01-04 23:41:16 +0100

    config files

--- sh/src/main/java/smarthome/Start.java
+++ sh/src/main/java/smarthome/Start.java
@@ -19,7 +19,8 @@ public class Start {
     public static void main(String[] args) {
         ObjectMapper mapper = new ObjectMapper();
 
-        String configFile = "houseconfig.json";
+        String configFile = "houseconfig.json"; // smaller house
+//        String configFile = "houseconfig2.json"; // bigger house
 
         try {
             InputStream inputStream = Start.class.getClassLoader().getResourceAsStream(configFile);
